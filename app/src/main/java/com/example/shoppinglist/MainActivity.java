c93dd44d45b3aa5377e869394fe7afc808eb92b0
Patrick Granberg <patgra91@gmail.com>
2020-10-03 16:44:30 +0200

    Can add items until all 10 TextViews are populated, state saved on onSaveInstanceState()

--- app/src/main/java/com/example/shoppinglist/MainActivity.java
+++ app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -8,22 +8,55 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     //private static final String EXTRA_MESSAGE = "com.example.shoppinglist.EXTRA.MESSAGE";
     private static final int REQUEST_CODE_SELECT_ITEM = 1;
+
+    private ArrayList<Integer> textViewIdArrayList = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*
+         * I'm sure there's a good way to list all Views within the layout and get their id's.
+         * Seems its not so trivial though, so I will do it like this:
+         *
+         * 1) Manually make a list of all TextViews.
+         * 2) Iterate through it and only add item if there are empty slots...
+         * */
+
+        // Keep track of id for all relevant TextViews.
+
+        if (textViewIdArrayList == null) {
+            textViewIdArrayList = new ArrayList<Integer>(10);
+            textViewIdArrayList.add(R.id.text_view_item_1);
+            textViewIdArrayList.add(R.id.text_view_item_2);
+            textViewIdArrayList.add(R.id.text_view_item_3);
+            textViewIdArrayList.add(R.id.text_view_item_4);
+            textViewIdArrayList.add(R.id.text_view_item_5);
+            textViewIdArrayList.add(R.id.text_view_item_6);
+            textViewIdArrayList.add(R.id.text_view_item_7);
+            textViewIdArrayList.add(R.id.text_view_item_8);
+            textViewIdArrayList.add(R.id.text_view_item_9);
+            textViewIdArrayList.add(R.id.text_view_item_10);
+        }
+
+        // Fill each TextView with the corresponding saved String.
+        TextView textView;
         if (savedInstanceState != null) {
-            // iterate: text_view_item_#
-            int i = 1;
-            TextView textView = findViewById(R.id.text_view_item_1);
-            textView.setText(savedInstanceState.getString(String.valueOf(i)));
+            for (Integer id : textViewIdArrayList) {
+                textView = (TextView) findViewById(id);
+                if (textView.getText().equals("")) {
+                    textView.setText(savedInstanceState.getString(String.valueOf(id)));
+                }
+            }
         }
     }
 
@@ -36,11 +69,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
         if (requestCode == REQUEST_CODE_SELECT_ITEM) {
             if (resultCode == RESULT_OK) {
-                TextView textView = findViewById(R.id.text_view_item_1);
-                textView.setText(data.getStringExtra(SelectItem.EXTRA_REPLY));
-                textView.setVisibility(View.VISIBLE);
+                // Iterate through TextViews, if empty: add newly selected String.
+                // Eventually, every TextView will be taken.
+                TextView textView = null;
+                for (Integer id : textViewIdArrayList){
+                    textView = (TextView) findViewById(id);
+                    if (textView.getText().equals("")){
+                        textView.setText(data.getStringExtra(SelectItem.EXTRA_REPLY));
+                        // Break out of the for loop, ony one new item is added at a time.
+                        break;
+                    }
+                }
             }
         }
     }
@@ -48,13 +90,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        TextView textView = findViewById(R.id.text_view_item_1);
-        // iterate: text_view_item_#
-
-        if (!textView.getText().equals("")) {
-            // save
-            int i = 1;
-            outState.putString(String.valueOf(i), textView.getText().toString());
+        TextView textView;
+        for (Integer id : textViewIdArrayList) {
+            textView = (TextView) findViewById(id);
+            if (!textView.getText().equals("")) {
+                outState.putString(String.valueOf(id), textView.getText().toString());
+            }
         }
     }
 }
